dca46a7fb217a3867745e1ed88061d5910d59a50
barahona1998MP <marcosplata98@gmail.com>
2024-06-26 15:14:34 -0500

    feat: add the controller implementation

--- src/main/java/com/mplatab/proyecto_todolist/controller/TodoController.java
+++ src/main/java/com/mplatab/proyecto_todolist/controller/TodoController.java
@@ -1,5 +1,49 @@
 package com.mplatab.proyecto_todolist.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mplatab.proyecto_todolist.entity.Todo;
+import com.mplatab.proyecto_todolist.service.ITodoService;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
-  
+
+  private final ITodoService todoService;
+
+  public TodoController(ITodoService todoService) {
+    this.todoService = todoService;
+  }
+
+  @PostMapping
+  List<Todo> create(@RequestBody Todo todo) {
+    return todoService.create(todo);
+  }
+
+  @GetMapping
+  List<Todo> list() {
+    return todoService.list();
+  }
+
+  @PutMapping()
+  List<Todo> update(@RequestBody Todo todo) {
+    return todoService.update(todo);
+  }
+
+
+  @DeleteMapping("/{id}")
+  List<Todo> delete(@PathVariable Long id) {
+    return todoService.delete(id);
+  }
+
+
 }
