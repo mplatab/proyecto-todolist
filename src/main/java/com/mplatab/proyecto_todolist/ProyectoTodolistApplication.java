1f821f8e973598d8b593cb707cd56cebcfa6a905
barahona1998MP <marcosplata98@gmail.com>
2024-06-26 14:38:58 -0500

    feat: add project structure

---
+++ src/main/java/com/mplatab/proyecto_todolist/ProyectoTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.mplatab.proyecto_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mplatab/proyecto_todolist/ProyectoTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mplatab.proyecto_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
